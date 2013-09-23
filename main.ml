e4ba62ba1668ae52009fc17e62ec86cf452e53ee
Reynir Reynisson <reynir@reynir.dk>
2013-09-23 15:04:33 +0200

    Alias the Digitalocean module in main.ml

--- main.ml
+++ main.ml
@@ -1,14 +1,16 @@
 module Json = Yojson.Safe
 open Http_client.Convenience
+open Util
+module DO = Digitalocean
 
 let pretty json =
   Json.pretty_to_string json
 
 let () =
-  let () = Digitalocean.init () in
-  let api = Util.load_api_from_file "../do.json" in
-  let droplets = Digitalocean.droplets api in
-  let regions = Digitalocean.regions api in
+  let () = DO.init () in
+  let api = load_api_from_file "../do.json" in
+  let droplets = DO.droplets api in
+  let regions = DO.regions api in
   print_endline (pretty droplets);
   print_endline (pretty regions);
-  print_endline (pretty (Digitalocean.domains api))
+  print_endline (pretty (DO.domains api))
