069056699f0818833775cc67cddea9bef11f38bb
Reynir Reynisson <reynir@reynir.dk>
2013-09-23 15:02:51 +0200

    Utility functions

--- main.ml
+++ main.ml
@@ -1,21 +1,12 @@
 module Json = Yojson.Safe
 open Http_client.Convenience
 
-let load_file f =
-  let ic = open_in f in
-  let n = in_channel_length ic in
-  let s = String.create n in
-  really_input ic s 0 n;
-  close_in ic;
-  s
-
 let pretty json =
   Json.pretty_to_string json
 
-
 let () =
   let () = Digitalocean.init () in
-  let api = ApiKey_j.api_of_string (load_file "../do.json") in
+  let api = Util.load_api_from_file "../do.json" in
   let droplets = Digitalocean.droplets api in
   let regions = Digitalocean.regions api in
   print_endline (pretty droplets);
---
+++ src/util.ml
@@ -0,0 +1,7 @@
+let load_api_from_file f =
+  let ic = open_in f in
+  let n = in_channel_length ic in
+  let s = String.create n in
+  really_input ic s 0 n;
+  close_in ic;
+  ApiKey_j.api_of_string s
